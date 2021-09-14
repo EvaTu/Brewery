b9d3a0f2aa744204923fa306d0595cfc4334d5b1
John Sangalang <sangalangjl@gmail.com>
2021-09-14 18:54:53 -0400

    Added data to db.json

--- my-app/src/components/Brewery.js
+++ my-app/src/components/Brewery.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
 function Brewery({ breweriesObject }) {
-    console.log(breweriesObject)
     const { image, name, street, postal_code, state, phone, website, details } = breweriesObject
 
     return (
         <div className="card">
-            <img src={image} alt={name}/>
+            <img className="cardImage" src={image} alt={name}/>
             <h3>{name}</h3>
             <h4>Address: {street} {postal_code}, {state}</h4>
             <h5>Phone #: {phone}</h5>
